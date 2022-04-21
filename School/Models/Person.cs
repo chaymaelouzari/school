283e498f376c145ebed09bf3babc703c77147ed9
chaymaelouzari <louzarichaymae@gmail.com>
2022-04-21 02:03:57 +0200

    sincronizando

---
+++ School/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace School.Models
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            CourseInstructors = new HashSet<CourseInstructor>();
+            StudentGrades = new HashSet<StudentGrade>();
+        }
+
+        public int PersonID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public DateTime? EnrollmentDate { get; set; }
+
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+        public virtual ICollection<CourseInstructor> CourseInstructors { get; set; }
+        public virtual ICollection<StudentGrade> StudentGrades { get; set; }
+    }
+}
--- School/Startup.cs
+++ School/Startup.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using School.Models;
 
 namespace School
 {
